71c282e018326a207b8a66fbee44198fd880c17a
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-08-12 15:27:31 +0900

    Comments are removed.

--- linux-sample/example.h
+++ linux-sample/example.h
@@ -9,18 +9,7 @@ const char EX_APP_ID[] = "00959619";
 const char EX_APP_KEY[] = "1ae0bcded44365e4f83c2daa2f4ca237";
 const char EX_APP_SITE[] = "api-development-jp.internal.kii.com";
 
-/* If two applications use same EX_AUTH_VENDOR_ID, receiving
- * notification does not work well. To avoid this situation, 3 vendor
- * thing ids are created. If you have some troubles in receiving
- * notification, the troubles may be resolved by changing vendor thing
- * id.
- *
- * If you change vendor thing id, you also need to change THING_ID in
- * script/smart-light-push.sh
- */
 const char EX_AUTH_VENDOR_ID[] = "4649";
-/* const char EX_AUTH_VENDOR_ID[] = "46490"; */
-/* const char EX_AUTH_VENDOR_ID[] = "46491"; */
 const char EX_AUTH_VENDOR_PASS[] = "1234";
 
 #define EX_COMMAND_HANDLER_BUFF_SIZE 4096
