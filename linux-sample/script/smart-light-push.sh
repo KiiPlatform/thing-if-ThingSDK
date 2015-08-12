eeb4aabfd0c60d6977805b602d08f6821f72ca9f
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-08-12 13:43:04 +0900

    smart-light-push.sh is fixed.

--- linux-sample/script/smart-light-push.sh
+++ linux-sample/script/smart-light-push.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
-
-APP_HOST=api-jp.kii.com
-APP_ID=a4c6c6a7
-APP_KEY=1154dcfd901bf1fdf5606d3b3d92e095
-# for 4649
-THING_ID=th.88eb20051321-7beb-5e11-c031-0096b1de
-# for 46490
-#THING_ID=th.88eb20051321-27da-5e11-d742-04089dbe
-# for 46491
-# THING_ID=th.dfa848a00022-8e09-5e11-1842-0d40c82b
-
-USER_ID=88eb20051321-7beb-5e11-9bd0-067e4ff6
+APP_ID=00959619
+APP_KEY=1ae0bcded44365e4f83c2daa2f4ca237
+APP_HOST=api-development-jp.internal.kii.com
+THING_ID=th.53ae324be5a0-bea9-5e11-96a3-0721cbd1
+ACCESS_TOKEN=gf59UOSCuvVJNddsXaLBs2yq107TlOB9ZrwifPEzCo0
+USER_ID=53ae324be5a0-bea9-5e11-86a3-0e9cbafc
 
 curl -v -X POST \
-  -H "X-Kii-AppID: ${APP_ID}" \
-  -H "X-Kii-AppKey: ${APP_KEY}" \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
   -H "Content-Type: application/json" \
-  "https://${APP_HOST}/api/apps/${APP_ID}/server-code/versions/current/postCommands" \
-  -d "{\"bodyJson\":{\"target\": \"${THING_ID}\",\"targetType\":\"thing\",\"schema\":\"SmartLightDemo\",\"schemaVersion\":1,\"actions\":[{\"turnPower\":{\"power\":true}},{\"setBrightness\":{\"brightness\":3000}},{\"setColor\":{\"color\":[0,128,255]}},{\"setColorTemperature\":{\"colorTemperature\":-100}}],\"failIfOffline\":false,\"issuer\":\"${USER_ID}\", \"issuerType\":\"user\"},\"headersJson\":{\"authorization\":\"meh\"}}"
+  "https://${APP_HOST}/iot-api/apps/${APP_ID}/targets/thing:${THING_ID}/commands" \
+  -d "{\"schemaVersion\":1,\"schema\":\"SmartLightDemo\",\"issuer\":\"user:${USER_ID}\",\"actions\":[{\"turnPower\":{\"power\":true}},{\"setBrightness\":{\"brightness\":3000}},{\"setColor\":{\"color\":[0,128,255]}},{\"setColorTemperature\":{\"colorTemperature\":-100}}]}"
+
