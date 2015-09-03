a25a9c53b708d8364baec5daf99c7b89e04124ba
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-09-03 15:18:04 +0900

    Function names are changed.

--- kii_iot_callbacks.h
+++ kii_iot_callbacks.h
@@ -8,7 +8,7 @@ extern "C" {
 #include <kii_socket_callback.h>
 #include <kii_task_callback.h>
 
-/** Callback for connecting command handler socket to server.
+/** Callback for connecting socket to HTTP or HTTPS server.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -19,12 +19,12 @@ extern "C" {
  * return KII_SOCKETC_FAIL. If applications want to pend returning
  * success or fail, applications need to return KII_SOCKETC_AGAIN.
  */
-kii_socket_code_t command_handler_connect_cb(
+kii_socket_code_t http_connect(
         kii_socket_context_t* socket_context,
         const char* host,
         unsigned int port);
 
-/** Callback for sending command handler data to server.
+/** Callback for sending data to HTTP or HTTPS server.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -37,12 +37,12 @@ kii_socket_code_t command_handler_connect_cb(
  * data, applications need to return KII_SOCKETC_AGAIN. In this case,
  * KiiThingSDK Embedded Core pass same data to this callback again.
  */
-kii_socket_code_t command_handler_send_cb(
+kii_socket_code_t http_send(
         kii_socket_context_t* socket_context,
         const char* buffer,
         size_t length);
 
-/** Callback for receiving command handler data from server.
+/** Callback for receiving data from HTTP or HTTPS server.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -57,13 +57,13 @@ kii_socket_code_t command_handler_send_cb(
  * KII_HTTPC_AGAIN. In this case, applications must not set receving
  * data to buffer if some data is received.
  */
-kii_socket_code_t command_handler_recv_cb(
+kii_socket_code_t http_recv(
         kii_socket_context_t* socket_context,
         char* buffer,
         size_t length_to_read,
         size_t* out_actual_length);
 
-/** Callback for closing command handler socket.
+/** Callback for closing HTTP or HTTPS socket.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -74,10 +74,10 @@ kii_socket_code_t command_handler_recv_cb(
  * to pend returning success or fail, applications need to return
  * KII_SOCKETC_AGAIN.
  */
-kii_socket_code_t command_handler_close_cb(
+kii_socket_code_t http_close(
         kii_socket_context_t* socket_context);
 
-/** Callback for connecting mqtt socket to server.
+/** Callback for connecting MQTT socket to server.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -88,12 +88,12 @@ kii_socket_code_t command_handler_close_cb(
  * return KII_SOCKETC_FAIL. If applications want to pend returning
  * success or fail, applications need to return KII_SOCKETC_AGAIN.
  */
-kii_socket_code_t mqtt_connect_cb(
+kii_socket_code_t mqtt_connect(
         kii_socket_context_t* socket_context,
         const char* host,
         unsigned int port);
 
-/** Callback for sending mqtt data to server.
+/** Callback for sending MQTT data to server.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -106,12 +106,12 @@ kii_socket_code_t mqtt_connect_cb(
  * data, applications need to return KII_SOCKETC_AGAIN. In this case,
  * KiiThingSDK Embedded Core pass same data to this callback again.
  */
-kii_socket_code_t mqtt_send_cb(
+kii_socket_code_t mqtt_send(
         kii_socket_context_t* socket_context,
         const char* buffer,
         size_t length);
 
-/** Callback for receiving mqtt data from server.
+/** Callback for receiving MQTT data from server.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -126,13 +126,13 @@ kii_socket_code_t mqtt_send_cb(
  * KII_HTTPC_AGAIN. In this case, applications must not set receving
  * data to buffer if some data is received.
  */
-kii_socket_code_t mqtt_recv_cb(
+kii_socket_code_t mqtt_recv(
         kii_socket_context_t* socket_context,
         char* buffer,
         size_t length_to_read,
         size_t* out_actual_length);
 
-/** Callback for closing mqtt socket.
+/** Callback for closing MQTT socket.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] socket_context context object.
@@ -143,77 +143,9 @@ kii_socket_code_t mqtt_recv_cb(
  * to pend returning success or fail, applications need to return
  * KII_SOCKETC_AGAIN.
  */
-kii_socket_code_t mqtt_close_cb(kii_socket_context_t* socket_context);
+kii_socket_code_t mqtt_close(kii_socket_context_t* socket_context);
 
-/** Callback for connecting state updater socket to server.
- * Applications must implement this callback in the target enviroment.
- *
- * @param [in] socket_context context object.
- * @param [in] host host name.
- * @param [in] port port number.
- * @return If connection is succeeded, applications need to return
- * KII_SOCKETC_OK. If connection is failed, applications need to
- * return KII_SOCKETC_FAIL. If applications want to pend returning
- * success or fail, applications need to return KII_SOCKETC_AGAIN.
- */
-kii_socket_code_t state_updater_connect_cb(
-        kii_socket_context_t* socket_context,
-        const char* host,
-        unsigned int port);
-
-/** Callback for sending state updater data to server.
- * Applications must implement this callback in the target enviroment.
- *
- * @param [in] socket_context context object.
- * @param [in] buffer data to send server.
- * @param [in] length length of buffer.
-
- * @return If applications succeed to send data, applications need to
- * return KII_SOCKETC_OK. If connection is failed. applications need
- * to return KII_SOCKETC__FAIL. If applications don't want to send
- * data, applications need to return KII_SOCKETC_AGAIN. In this case,
- * KiiThingSDK Embedded Core pass same data to this callback again.
- */
-kii_socket_code_t state_updater_send_cb(
-        kii_socket_context_t* socket_context,
-        const char* buffer,
-        size_t length);
-
-/** Callback for receiving state updater data from server.
- * Applications must implement this callback in the target enviroment.
- *
- * @param [in] socket_context context object.
- * @param [out] buffer buffer to set receiving data.
- * @param [in] length_to_read buffer size.
- * @param [out] out_actual_length actual set data size.
- * @return If applications succeed to receive data and set the data to
- * buffer, applications need to return KII_SOCKETC_OK. Applications
- * also set data size to out_actual_length. If applications fail,
- * applications need to return KII_HTTPC_FAIL. If applications want to
- * wait to receive data, applications need to return
- * KII_HTTPC_AGAIN. In this case, applications must not set receving
- * data to buffer if some data is received.
- */
-kii_socket_code_t state_updater_recv_cb(
-        kii_socket_context_t* socket_context,
-        char* buffer,
-        size_t length_to_read,
-        size_t* out_actual_length);
-
-/** Callback for closing state updater socket.
- * Applications must implement this callback in the target enviroment.
- *
- * @param [in] socket_context context object.
- *
- * @return If applications succeed to close socket, applications need
- * to return KII_SOCKETC_OK. If applications fail to close socket,
- * applications need to return KII_SOCKETC_FAIL. If applications want
- * to pend returning success or fail, applications need to return
- * KII_SOCKETC_AGAIN.
- */
-kii_socket_code_t state_updater_close_cb(kii_socket_context_t* socket_context);
-
-/** Callback for creating command handler task.
+/** Callback for creating task.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] name name of task.
@@ -224,7 +156,7 @@ kii_socket_code_t state_updater_close_cb(kii_socket_context_t* socket_context);
  *
  * @return KII_TASKC_OK if succeed to create task. otherwise KII_TASKC_FAIL.
  */
-kii_task_code_t command_handler_task_create_cb(
+kii_task_code_t task_create(
         const char* name,
         KII_TASK_ENTRY entry,
         void* param,
@@ -232,45 +164,19 @@ kii_task_code_t command_handler_task_create_cb(
         unsigned int stk_size,
         unsigned int priority);
 
-/** Callback for delaying command handler process.
+/** Callback for delaying task.
  * Applications must implement this callback in the target enviroment.
  *
  * @param[in] msec millisecond to delay.
  */
-void command_handler_delay_ms_cb(unsigned int msec);
-
-/** Callback for creating state updater task.
- * Applications must implement this callback in the target enviroment.
- *
- * @param [in] name name of task.
- * @param [in] entry entry of task.
- * @param [in] stk_start start position of stack area.
- * @param [in] stk_size stack size of task
- * @param [in] priority priority of thisk
- *
- * @return KII_TASKC_OK if succeed to create task. otherwise KII_TASKC_FAIL.
- */
-kii_task_code_t state_updater_task_create_cb(
-        const char* name,
-        KII_TASK_ENTRY entry,
-        void* param,
-        unsigned char* stk_start,
-        unsigned int stk_size,
-        unsigned int priority);
-
-/** Callback for delaying state updater process.
- * Applications must implement this callback in the target enviroment.
- *
- * @param[in] msec millisecond to delay.
- */
-void state_updater_delay_ms_cb(unsigned int msec);
+void delay_ms(unsigned int msec);
 
 /** Callback for logging kii iot.
  * Applications must implement this callback in the target enviroment.
  *
  * @param [in] format outputted string format.
  */
-void logger_cb(const char* format, ...);
+void logger(const char* format, ...);
 
 #ifdef __cplusplus
 }
--- linux-sample/kii_iot_callbacks_impl.c
+++ linux-sample/kii_iot_callbacks_impl.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-kii_socket_code_t command_handler_connect_cb(
+kii_socket_code_t http_connect(
         kii_socket_context_t* socket_context,
         const char* host,
         unsigned int port)
@@ -15,7 +15,7 @@ kii_socket_code_t command_handler_connect_cb(
     return s_connect_cb(socket_context, host, port);
 }
 
-kii_socket_code_t command_handler_send_cb(
+kii_socket_code_t http_send(
         kii_socket_context_t* socket_context,
         const char* buffer,
         size_t length)
@@ -23,7 +23,7 @@ kii_socket_code_t command_handler_send_cb(
     return s_send_cb(socket_context, buffer, length);
 }
 
-kii_socket_code_t command_handler_recv_cb(
+kii_socket_code_t http_recv(
         kii_socket_context_t* socket_context,
         char* buffer,
         size_t length_to_read,
@@ -32,12 +32,12 @@ kii_socket_code_t command_handler_recv_cb(
     return s_recv_cb(socket_context, buffer, length_to_read, out_actual_length);
 }
 
-kii_socket_code_t command_handler_close_cb(kii_socket_context_t* socket_context)
+kii_socket_code_t http_close(kii_socket_context_t* socket_context)
 {
     return s_close_cb(socket_context);
 }
 
-kii_socket_code_t mqtt_connect_cb(
+kii_socket_code_t mqtt_connect(
         kii_socket_context_t* socket_context,
         const char* host,
         unsigned int port)
@@ -45,7 +45,7 @@ kii_socket_code_t mqtt_connect_cb(
     return connect_cb(socket_context, host, port);
 }
 
-kii_socket_code_t mqtt_send_cb(
+kii_socket_code_t mqtt_send(
         kii_socket_context_t* socket_context,
         const char* buffer,
         size_t length)
@@ -53,7 +53,7 @@ kii_socket_code_t mqtt_send_cb(
     return send_cb(socket_context, buffer, length);
 }
 
-kii_socket_code_t mqtt_recv_cb(
+kii_socket_code_t mqtt_recv(
         kii_socket_context_t* socket_context,
         char* buffer,
         size_t length_to_read,
@@ -62,42 +62,12 @@ kii_socket_code_t mqtt_recv_cb(
     return recv_cb(socket_context, buffer, length_to_read, out_actual_length);
 }
 
-kii_socket_code_t mqtt_close_cb(kii_socket_context_t* socket_context)
+kii_socket_code_t mqtt_close(kii_socket_context_t* socket_context)
 {
     return close_cb(socket_context);
 }
 
-kii_socket_code_t state_updater_connect_cb(
-        kii_socket_context_t* socket_context,
-        const char* host,
-        unsigned int port)
-{
-    return s_connect_cb(socket_context, host, port);
-}
-
-kii_socket_code_t state_updater_send_cb(
-        kii_socket_context_t* socket_context,
-        const char* buffer,
-        size_t length)
-{
-    return s_send_cb(socket_context, buffer, length);
-}
-
-kii_socket_code_t state_updater_recv_cb(
-        kii_socket_context_t* socket_context,
-        char* buffer,
-        size_t length_to_read,
-        size_t* out_actual_length)
-{
-    return s_recv_cb(socket_context, buffer, length_to_read, out_actual_length);
-}
-
-kii_socket_code_t state_updater_close_cb(kii_socket_context_t* socket_context)
-{
-    return s_close_cb(socket_context);
-}
-
-kii_task_code_t command_handler_task_create_cb(
+kii_task_code_t task_create(
         const char* name,
         KII_TASK_ENTRY entry,
         void* param,
@@ -108,28 +78,12 @@ kii_task_code_t command_handler_task_create_cb(
     return task_create_cb(name, entry, param, stk_start, stk_size, priority);
 }
 
-void command_handler_delay_ms_cb(unsigned int msec)
+void delay_ms(unsigned int msec)
 {
     delay_ms_cb(msec);
 }
 
-kii_task_code_t state_updater_task_create_cb(
-        const char* name,
-        KII_TASK_ENTRY entry,
-        void* param,
-        unsigned char* stk_start,
-        unsigned int stk_size,
-        unsigned int priority)
-{
-    return task_create_cb(name, entry, param, stk_start, stk_size, priority);
-}
-
-void state_updater_delay_ms_cb(unsigned int msec)
-{
-    delay_ms_cb(msec);
-}
-
-void logger_cb(const char* format, ...)
+void logger(const char* format, ...)
 {
     va_list list;
     va_start(list, format);
