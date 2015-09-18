90ef48bb3d337955c4428c85d1927f2105138b92
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-09-18 17:50:09 +0900

    Vendor thing ID for sample application is changed.

--- linux-sample/example.h
+++ linux-sample/example.h
@@ -9,11 +9,11 @@ const char EX_APP_ID[] = "00959619";
 const char EX_APP_KEY[] = "1ae0bcded44365e4f83c2daa2f4ca237";
 const char EX_APP_SITE[] = "api-development-jp.internal.kii.com";
 
-const char EX_AUTH_VENDOR_ID[] = "4649";
+const char EX_AUTH_VENDOR_ID[] = "464939";
 const char EX_AUTH_VENDOR_PASS[] = "1234";
 
-const char EX_AUTH_THING_ID[] = "th.53ae324be5a0-bea9-5e11-96a3-0721cbd1";
-const char EX_ACCESS_TOKEN[] = "gf59UOSCuvVJNddsXaLBs2yq107TlOB9ZrwifPEzCo0";
+const char EX_AUTH_THING_ID[] = "th.53ae324be5a0-f5e8-5e11-fdd5-000ee263";
+const char EX_ACCESS_TOKEN[] = "7H2sc6UYvC2mKCFihYhA52xh1b--G3rga_OYz_gbCg4";
 
 #define EX_COMMAND_HANDLER_BUFF_SIZE 4096
 #define EX_STATE_UPDATER_BUFF_SIZE 4096
