840c1430bea117ad497100e537aa37489a3ff8c2
satoshi <satoshi.kumano@kii.com>
2015-10-27 19:01:54 +0900

    Switched app.

--- linux-sample/example.h
+++ linux-sample/example.h
@@ -5,9 +5,9 @@
 extern 'C' {
 #endif
 
-const char EX_APP_ID[] = "00959619";
-const char EX_APP_KEY[] = "1ae0bcded44365e4f83c2daa2f4ca237";
-const char EX_APP_SITE[] = "api-development-jp.internal.kii.com";
+const char EX_APP_ID[] = "b8d819c8";
+const char EX_APP_KEY[] = "bff3bfdc1e3fcf6818919144330a0770";
+const char EX_APP_SITE[] = "api-jp.kii.com";
 
 #define EX_COMMAND_HANDLER_BUFF_SIZE 4096
 #define EX_STATE_UPDATER_BUFF_SIZE 4096
