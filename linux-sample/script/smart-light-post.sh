44e23a7e440fa7cbf86f4ef4cc69c4ca10d32d9b
satoshi <satoshi.kumano@kii.com>
2015-10-27 20:46:12 +0900

    Removed fixed variable definition.

--- linux-sample/script/smart-light-post.sh
+++ linux-sample/script/smart-light-post.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-APP_ID=00959619
-APP_KEY=1ae0bcded44365e4f83c2daa2f4ca237
-APP_HOST=api-development-jp.internal.kii.com
-THING_ID=th.53ae324be5a0-4908-5e11-a427-010b4f79
-ACCESS_TOKEN=4JPctYftnTzFgCwKWMZFuzcZ5v-JMEckI1_ynb6eFEg
-USER_ID=53ae324be5a0-bea9-5e11-86a3-0e9cbafc
-
 curl -v -X POST \
   -H "Authorization: Bearer ${ACCESS_TOKEN}" \
   -H "Content-Type: application/json" \
