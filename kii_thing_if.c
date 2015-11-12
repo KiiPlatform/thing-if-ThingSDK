918c2f3803c3d158847dfc2e409a869a182a8170
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-11-12 15:06:14 +0900

    Undo.

--- kii_thing_if.c
+++ kii_thing_if.c
@@ -260,52 +260,6 @@ static int prv_kii_thing_if_get_key_and_value_from_json(
     }
 }
 
-static kii_bool_t prv_writer(kii_t* kii, const char* buff)
-{
-    if (kii_api_call_append_body(kii, buff, strlen(buff)) != 0) {
-        M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-        return KII_FALSE;
-    }
-    return KII_TRUE;
-}
-
-static void prv_send_status(kii_t* kii)
-{
-    char resource_path[256];
-
-    if (sizeof(resource_path) / sizeof(resource_path[0]) <=
-            CONST_STRLEN(THING_IF_APP_PATH) +
-            strlen(kii->kii_core.app_id) + CONST_STRLEN(TARGET_PART) +
-            strlen(kii->kii_core.author.author_id) +
-            CONST_STRLEN(STATES_PART)) {
-        M_KII_LOG(kii->kii_core.logger_cb(
-                "resource path is longer than expected.\n"));
-        return;
-    }
-
-    resource_path[0] = '\0';
-    strcat(resource_path, THING_IF_APP_PATH);
-    strcat(resource_path, kii->kii_core.app_id);
-    strcat(resource_path, TARGET_PART);
-    strcat(resource_path, kii->kii_core.author.author_id);
-    strcat(resource_path, STATES_PART);
-
-    if (kii_api_call_start(kii, "PUT", resource_path, CONTENT_TYPE_JSON,
-                    KII_TRUE) != 0) {
-        M_KII_LOG(kii->kii_core.logger_cb("fail to start api call.\n"));
-        return;
-    }
-    if (((kii_thing_if_t*)kii->app_context)->state_handler(kii, prv_writer)
-            == KII_FALSE) {
-        M_KII_LOG(kii->kii_core.logger_cb("fail to start api call.\n"));
-        return;
-    }
-    if (kii_api_call_run(kii) != 0) {
-        M_KII_LOG(kii->kii_core.logger_cb("fail to run api.\n"));
-        return;
-    }
-}
-
 static void received_callback(kii_t* kii, char* buffer, size_t buffer_size) {
     kii_json_field_t fields[6];
     kii_json_field_t action[2];
@@ -316,7 +270,6 @@ static void received_callback(kii_t* kii, char* buffer, size_t buffer_size) {
     char index[ULONGBUFSIZE];
     size_t i = 0;
     char resource_path[256];
-    kii_bool_t all_success = KII_TRUE;
 
     memset(fields, 0x00, sizeof(fields));
     fields[0].path = "/schema";
@@ -444,7 +397,6 @@ static void received_callback(kii_t* kii, char* buffer, size_t buffer_size) {
                         return;
                     }
                 } else {
-                    all_success = KII_FALSE;
                     if (kii_api_call_append_body(kii,
                                     "{\"", sizeof("{\"") - 1) != 0) {
                         M_KII_LOG(kii->kii_core.logger_cb(
@@ -500,9 +452,6 @@ static void received_callback(kii_t* kii, char* buffer, size_t buffer_size) {
         return;
     }
 
-    if (all_success != KII_FALSE) {
-        prv_send_status(kii);
-    }
     return;
 }
 
@@ -683,6 +632,15 @@ static kii_bool_t prv_onboard_with_vendor_thing_id(
         return KII_FALSE;
     }
 
+    return KII_TRUE;
+}
+
+static kii_bool_t prv_writer(kii_t* kii, const char* buff)
+{
+    if (kii_api_call_append_body(kii, buff, strlen(buff)) != 0) {
+        M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
+        return KII_FALSE;
+    }
     return KII_TRUE;
 }
 
