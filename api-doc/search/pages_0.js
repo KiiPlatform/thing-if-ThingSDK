8c15d808805e12461c0f69ade37f8261ddaad99a
satoshi kumano <satoshi.kumano@kii.com>
2017-01-26 19:06:15 +0900

    updated doc

---
+++ api-doc/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['kii_20thing_2dif_20sdk_20for_20thing',['Kii thing-if SDK for Thing',['../index.html',1,'']]]
+];
---
+++ api-doc/search/typedefs_0.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['kii_5fthing_5fif_5faction_5fhandler',['KII_THING_IF_ACTION_HANDLER',['../kii__thing__if_8h.html#afef4118a3553cdcb7327b79f6907ae72',1,'kii_thing_if.h']]],
+  ['kii_5fthing_5fif_5fcommand_5fhandler_5fresource_5ft',['kii_thing_if_command_handler_resource_t',['../kii__thing__if_8h.html#ad75164fda1373ff006c19028db6f9016',1,'kii_thing_if.h']]],
+  ['kii_5fthing_5fif_5fcustom_5fpush_5fhandler',['KII_THING_IF_CUSTOM_PUSH_HANDLER',['../kii__thing__if_8h.html#ab52b93d0478b6561282d8b2f86b50b09',1,'kii_thing_if.h']]],
+  ['kii_5fthing_5fif_5fstate_5fhandler',['KII_THING_IF_STATE_HANDLER',['../kii__thing__if_8h.html#a9ffcbcf90bce33317ebe66b517270e50',1,'kii_thing_if.h']]],
+  ['kii_5fthing_5fif_5fstate_5fupdater_5fresource_5ft',['kii_thing_if_state_updater_resource_t',['../kii__thing__if_8h.html#af8d4493927ac3ece4225af0b84863625',1,'kii_thing_if.h']]],
+  ['kii_5fthing_5fif_5ft',['kii_thing_if_t',['../kii__thing__if_8h.html#aac6c8c3dded3493acca81cfeb87e2480',1,'kii_thing_if.h']]],
+  ['kii_5fthing_5fif_5fwriter',['KII_THING_IF_WRITER',['../kii__thing__if_8h.html#add8fbfbcca4efdbfc1cec0d965737721',1,'kii_thing_if.h']]]
+];
