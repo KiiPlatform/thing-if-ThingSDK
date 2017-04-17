afb89e6661668ee797d7eff151bc265db2af78a7
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-04-17 18:38:43 +0900

    Refactor adding key-value pair.

--- kii_thing_if.c
+++ kii_thing_if.c
@@ -37,6 +37,60 @@
 
 #define THING_IF_INFO "sn=tic;sv=0.9.6"
 
+typedef enum successor_t {
+    NOT_SUCCESSOR,
+    IS_SUCCESSOR
+} successor_t;
+
+static int prv_append_key_value_string(
+        kii_t* kii,
+        const char* key,
+        const char* value,
+        successor_t successor)
+{
+    if (key == NULL) {
+        M_KII_LOG(kii->kii_core.logger_cb("key not specified.\n"));
+        return -1;
+    }
+    if (value == NULL) {
+        M_KII_LOG(
+            kii->kii_core.logger_cb("value not specified for key: %s.\n", key));
+        return -1;
+    }
+
+    if (successor == IS_SUCCESSOR) {
+        if (kii_api_call_append_body(kii, ",", CONST_STRLEN(",") != 0)) {
+            M_KII_LOG(kii->kii_core.logger_cb(
+                "request size overflowed: (%s, %s).\n", key, value));
+            return -1;
+        }
+    }
+    if (kii_api_call_append_body(kii, "\"", CONST_STRLEN("\"")) != 0 ||
+            kii_api_call_append_body(kii, key, strlen(key)) != 0 ||
+            kii_api_call_append_body(
+                kii, "\":\"", CONST_STRLEN("\":\"")) != 0 ||
+            kii_api_call_append_body(kii, value, strlen(value)) != 0 ||
+            kii_api_call_append_body(kii, "\"", CONST_STRLEN("\"")) != 0) {
+        M_KII_LOG(kii->kii_core.logger_cb(
+            "request size overflowed: (%s, %s).\n", key, value));
+        return -1;
+    }
+    return 0;
+}
+
+static int prv_append_key_value_string_optional(
+        kii_t* kii,
+        const char* key,
+        const char* value,
+        successor_t successor)
+{
+    if (key == NULL || value == NULL) {
+        return 0;
+    }
+    return prv_append_key_value_string(kii, key, value, successor);
+}
+
+
 static kii_json_parse_result_t prv_kii_thing_if_json_read_object(
         kii_t* kii,
         const char* json_string,
@@ -634,82 +688,28 @@ static kii_bool_t prv_onboard_with_vendor_thing_id(
         M_KII_LOG(kii->kii_core.logger_cb("fail to start api call.\n"));
     }
 
-    if (kii_api_call_append_body(kii, "{\"vendorThingID\":\"",
-                    CONST_STRLEN("{\"vendorThingID\":\"")) != 0) {
+    // Open to write JSON object.
+    if (kii_api_call_append_body(kii, "{", CONST_STRLEN("{")) != 0) {
         M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
         return KII_FALSE;
     }
-    if (kii_api_call_append_body(kii, vendor_thing_id,
-                    strlen(vendor_thing_id)) != 0) {
+    // Append key value pairs.
+    if (prv_append_key_value_string(
+            kii, "vendorThingID", vendor_thing_id, NOT_SUCCESSOR) != 0 ||
+            prv_append_key_value_string(
+                kii, "thingPassword", password, IS_SUCCESSOR) != 0 ||
+            prv_append_key_value_string_optional(
+                kii, "thingType", thing_type, IS_SUCCESSOR) != 0 ||
+            prv_append_key_value_string_optional(
+                kii, "thingProperties", thing_properties, IS_SUCCESSOR) != 0 ||
+            prv_append_key_value_string_optional(
+                kii, "firmwareVersion", firmware_version, IS_SUCCESSOR) != 0 ||
+            prv_append_key_value_string_optional(
+                kii, "layoutPosition", layout_position, IS_SUCCESSOR) != 0) {
         M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
         return KII_FALSE;
     }
-    if (kii_api_call_append_body(kii, "\",\"thingPassword\":\"",
-                    CONST_STRLEN("\",\"thingPassword\":\"")) != 0) {
-        M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-        return KII_FALSE;
-    }
-    if (kii_api_call_append_body(kii, password, strlen(password)) != 0) {
-        M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-        return KII_FALSE;
-    }
-    if (kii_api_call_append_body(kii, "\"", CONST_STRLEN("\"")) != 0) {
-        M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-        return KII_FALSE;
-    }
-
-    if (thing_type != NULL) {
-        if (kii_api_call_append_body(kii, ",\"thingType\":\"",
-                        CONST_STRLEN(",\"thingType\":\"")) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-        if (kii_api_call_append_body(kii, thing_type, strlen(thing_type))
-                != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-        if (kii_api_call_append_body(kii, "\"", CONST_STRLEN("\"")) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-    }
-    if (thing_properties != NULL) {
-        if (kii_api_call_append_body(kii, ",\"thingProperties\":",
-                        CONST_STRLEN(",\"thingProperties\":")) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-        if (kii_api_call_append_body(kii, thing_properties,
-                        strlen(thing_properties)) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-    }
-    if (firmware_version != NULL) {
-        if (kii_api_call_append_body(kii, ",\"firmwareVersion\":",
-                        CONST_STRLEN(",\"firmwareVersion\":")) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-        if (kii_api_call_append_body(kii, firmware_version,
-                        strlen(firmware_version)) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-    }
-    if (layout_position != NULL) {
-        if (kii_api_call_append_body(kii, ",\"layoutPosition\":",
-                        CONST_STRLEN(",\"layoutPosition\":")) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-        if (kii_api_call_append_body(kii, layout_position,
-                        strlen(layout_position)) != 0) {
-            M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
-            return KII_FALSE;
-        }
-    }
+    // Close JSON object.
     if (kii_api_call_append_body(kii, "}", CONST_STRLEN("}")) != 0) {
         M_KII_LOG(kii->kii_core.logger_cb("request size overflowed.\n"));
         return KII_FALSE;
