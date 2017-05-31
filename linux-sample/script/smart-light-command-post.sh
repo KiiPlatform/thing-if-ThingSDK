ef4b304771f178cef83353d6f10c2da9c24bf26c
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-05-31 15:58:38 +0900

    Fix smart-light-command-post.sh.

--- linux-sample/script/smart-light-command-post.sh
+++ linux-sample/script/smart-light-command-post.sh
@@ -2,7 +2,6 @@
 
 curl -v -X POST \
   -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-  -H "Content-Type: application/json" \
+  -H "Content-Type: application/vnd.kii.CommandCreationRequest+json" \
   "https://${APP_HOST}/thing-if/apps/${APP_ID}/targets/thing:${THING_ID}/commands" \
-  -d "{\"schemaVersion\":1,\"schema\":\"SmartLightDemo\",\"issuer\":\"user:${USER_ID}\",\"actions\":[{\"turnPower\":{\"power\":true}},{\"setBrightness\":{\"brightness\":3000}},{\"setColor\":{\"color\":[0,128,255]}},{\"setColorTemperature\":{\"colorTemperature\":-100}}]}"
-
+  -d "{\"issuer\":\"user:${USER_ID}\",\"actions\":[{\"AirConditionerAlias\":[{\"turnPower\":true}]}]}"
