1181f4da8178ab8c374b1dbf9ed6686845bd5336
satoshikumano <satoshi.kumano@kii.com>
2018-03-06 19:38:01 +0900

    Added ARM Toolchain file.

---
+++ arm-toolchain.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc)
+link_directories(/home/satoshi/arm-local/lib)
+include_directories(/home/satoshi/arm-local/include)
