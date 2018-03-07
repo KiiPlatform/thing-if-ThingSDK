817df2afa1f6e16b9becbbeb7295ff479ae8ca41
satoshikumano <satoshi.kumano@kii.com>
2018-03-07 13:36:03 +0900

    Specified ARM version.

--- arm-toolchain.cmake
+++ arm-toolchain.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR armv5te)
 
 set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc)
 link_directories(/home/satoshi/arm-local/lib)
