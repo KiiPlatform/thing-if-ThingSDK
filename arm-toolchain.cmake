3562e3bac3149a822061302a7ba1575aeedbf2cb
satoshikumano <satoshi.kumano@kii.com>
2018-03-07 16:18:04 +0900

    Imporved toolchain file.

--- arm-toolchain.cmake
+++ arm-toolchain.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR armv5te)
+set(CMAKE_SYSTEM_PROCESSOR armv5t)
 
 set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc)
-link_directories(/home/satoshi/arm-local/lib)
-include_directories(/home/satoshi/arm-local/include)
+
+link_directories($ENV{HOME}/arm-local/lib)
+include_directories($ENV{HOME}/arm-local/include)
