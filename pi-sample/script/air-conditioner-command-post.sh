9083842f0c79fc26b9ad0af87803782bc444f4cd
yongpingchen <yongping.chen@kii.com>
2018-08-08 19:36:46 +0900

    remove script

--- pi-sample/script/air-conditioner-command-post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -v -X POST \
-  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-  -H "Content-Type: application/vnd.kii.CommandCreationRequest+json" \
-  "https://${APP_HOST}/thing-if/apps/${APP_ID}/targets/thing:${THING_ID}/commands" \
-  -d "{\"issuer\":\"user:${USER_ID}\",\"actions\":[{\"AirConditionerAlias\":[{\"turnPower\":true},{\"setPresetTemperature\":25}]}]}"
--- pi-sample/script/create_topic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -v -X PUT \
-  -H "Authorization: Bearer ${THING_ACCESS_TOKEN}" \
-  "https://${APP_HOST}/api/apps/${APP_ID}/things/${THING_ID}/topics/${TOPIC_ID}"
